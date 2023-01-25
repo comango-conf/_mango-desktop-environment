a9a7a72f15f0b3a7696d9cbc5973e5d542981637
Hannah Lappe <hannah@lappe.page>
2023-01-25 09:42:05 +0100

    auto update

--- nix/modules/vim.nix
+++ nix/modules/vim.nix
@@ -1,20 +1,28 @@
 { pkgs, ... }:
 
 
-{
-  
+{  
   environment.systemPackages = with pkgs; [
     ((vim_configurable.override {  }).customize{
       name = "vim";
       # Install plugins for example for syntax highlighting of nix files
       vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
-        start = [ vim-nix vim-lastplace ];
+        start = [ vim-nix vim-lastplace intendLine ];
         opt = [];
       };
       vimrcConfig.customRC = ''
         " your custom vimrc
         set nocompatible
         set backspace=indent,eol,start
+
+	set number
+        set relativenumber
+
+        set autoindent
+        set expandtab
+        set shiftwidth=2
+        set softtabstop=2
+        set smarttab
         " Turn on syntax highlighting by default
         syntax on
         " ...
