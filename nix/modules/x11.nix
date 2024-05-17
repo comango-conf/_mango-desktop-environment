8eb2bd373c2e6933f52d879215b2358ef93e6a54
HanLap <hannah@lappe.page>
2024-05-17 21:36:18 +0200

    auto update

--- nix/modules/x11.nix
+++ nix/modules/x11.nix
@@ -7,21 +7,6 @@
 
     deviceSection = ''Option "TearFree" "true"''; # For amdgpu.
 
-    displayManager = {
-       gdm.enable = true;
-       sddm.enable = false;
-       lightdm.enable = false;
-      # defaultSession = "hyprland";
-    };
-    desktopManager = {
-      plasma5.enable = false;
-      gnome.enable = false; 
-    };
-
-    windowManager = {
-      awesome.enable = true;
-    };
-
     xkb = {
       layout = "us";
       model = "pc104";
